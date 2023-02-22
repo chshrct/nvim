27bc33266f2d7acc4586e50da06859f3a1abdd0d
Yauheni Kastrama <eugene.kostroma@gmail.com>
2023-02-22 13:20:31 +0400

    treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,38 +4,13 @@ return {
     version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        init = function()
-          -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-          local plugin =
-            require("lazy.core.config").spec.plugins["nvim-treesitter"]
-          local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-          local enabled = false
-          if opts.textobjects then
-            for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-              if opts.textobjects[mod] and opts.textobjects[mod].enable then
-                enabled = true
-                break
-              end
-            end
-          end
-          if not enabled then
-            require("lazy.core.loader").disable_rtp_plugin(
-              "nvim-treesitter-textobjects"
-            )
-          end
-        end,
-      },
-    },
     keys = {
-      { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Schrink selection", mode = "x" },
+      { "<leader>v", desc = "Increment selection" },
+      { "<leader>V", desc = "Schrink selection", mode = "x" },
     },
 
     opts = {
-      highlight = { enable = true },
+      highlight = { enable = true, use_languagetree = true },
       indent = { enable = true, disable = { "python" } },
       context_commentstring = { enable = true, enable_autocmd = false },
       ensure_installed = {
@@ -49,10 +24,10 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
+          init_selection = "<leader>v",
+          node_incremental = "<leader>v",
           scope_incremental = "<nop>",
-          node_decremental = "<bs>",
+          node_decremental = "<leader>V",
         },
       },
     },
