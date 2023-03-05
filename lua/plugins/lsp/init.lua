ba9fcaab9176996c2a8879b89a11e820fd3d61e3
Yauheni Kastrama <eugene.kostroma@gmail.com>
2023-03-05 18:48:09 +0400

    lsp setup

---
+++ lua/plugins/lsp/init.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    event = "BufReadPre",
+    dependencies = {
+      { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+      {
+        "folke/neodev.nvim",
+        opts = {
+          library = { plugins = { "neotest", "nvim-dap-ui" }, types = true },
+        },
+      },
+      { "j-hui/fidget.nvim", config = true },
+      { "smjonas/inc-rename.nvim", config = true },
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+    },
+    opts = {
+      servers = {},
+      setup = {},
+    },
+    config = function(plugin, opts)
+      require("plugins.lsp.servers").setup(plugin, opts)
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "ruff",
+        "debugpy",
+        "codelldb",
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup()
+      local mr = require("mason-registry")
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "BufReadPre",
+    dependencies = { "mason.nvim" },
+    config = function()
+      local nls = require("null-ls")
+      nls.setup({
+        sources = {
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.black,
+          nls.builtins.diagnostics.ruff.with({
+            extra_args = { "--max-line-length=180" },
+          }),
+        },
+      })
+    end,
+  },
+  {
+    "glepnir/lspsaga.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
+}
