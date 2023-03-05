9947891d014be4a2b566cdcb8c780e46bee2220f
Yauheni Kastrama <eugene.kostroma@gmail.com>
2023-03-05 13:39:26 +0400

    lualine lazy

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -75,55 +75,54 @@ return {
   -- status line
   {
     "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
     dependencies = {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       opt = true,
     },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "auto",
-          globalstatus = true,
-          disabled_filetypes = { statusline = { "lazy" } },
-          icons_enabled = true,
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = {
-            {
-              "filetype",
-              icon_only = true,
-              separator = "",
-              padding = { left = 1, right = 0 },
-            },
-            {
-              "filename",
-              path = 1,
-              symbols = { modified = "  ", readonly = "", unnamed = "" },
-            },
+    opts = {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "lazy" } },
+        icons_enabled = true,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          {
+            "filetype",
+            icon_only = true,
+            separator = "",
+            padding = { left = 1, right = 0 },
           },
-          lualine_x = {
-            "encoding",
-            {
-              "fileformat",
-            },
-          },
-          lualine_y = {
-            {
-              "progress",
-              separator = "|",
-            },
-            { "location", padding = { left = 0, right = 1 } },
-          },
-          lualine_z = {
-            function()
-              return " " .. os.date("%R")
-            end,
+          {
+            "filename",
+            path = 1,
+            symbols = { modified = "  ", readonly = "", unnamed = "" },
           },
         },
-        extensions = { "neo-tree" },
-      })
-    end,
+        lualine_x = {
+          "encoding",
+          {
+            "fileformat",
+          },
+        },
+        lualine_y = {
+          {
+            "progress",
+            separator = "|",
+          },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
+      },
+      extensions = { "neo-tree" },
+    },
   },
 }
