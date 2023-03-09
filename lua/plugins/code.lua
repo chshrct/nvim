f00a2227014426e01a9b89933e63b5d601290460
Yauheni Kastrama <eugene.kostroma@gmail.com>
2023-03-09 12:05:36 +0400

    change comment plugin

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -56,20 +56,10 @@ return {
   },
 
   -- comments
-  { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
   {
-    "echasnovski/mini.comment",
+    "numToStr/Comment.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      hooks = {
-        pre = function()
-          require("ts_context_commentstring.internal").update_commentstring({})
-        end,
-      },
-    },
-    config = function(_, opts)
-      require("mini.comment").setup(opts)
-    end,
+    opts = {},
   },
 
   -- surround
